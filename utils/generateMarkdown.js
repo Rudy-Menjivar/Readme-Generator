fdc59e9a8b61ae6b9c9f293fddc2732311640e4f
Rudy-Menjivar <rudy.menjivar@gmail.com>
2020-12-07 13:12:49 -0800

    updated contributing function to include more instructions under separate ordered lines

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,23 @@
 function standardContributing(data) {
   if (data.contributing == "Yes") {
     return `To participate in this project, please review the following guidelines:
-    * [Submit feature requests and bugs](https://github.com/${data.github}/${data.title}/issues)` 
+    \n1. Create a branch for your update (after forking and cloning)
+    \`\`\`
+    git checkout -b <branchName>
+    \`\`\`
+    \n2. Make changes or additions to new or existing file & stage it
+    \`\`\`
+    git add <fileName.ext>
+    \`\`\`
+    \n3. Commit your file by adding comments about code enhancements
+    \`\`\`
+    git commmit -m <your code comments>
+    \`\`\`
+    \n4. Push your changes with your remote branch name 
+    \`\`\`
+    git push -u origin <branchName>
+    \`\`\`
+    \n5. Finally, submit [feature requests and bugs](https://github.com/${data.github}/${data.title}/issues) or open a [pull request](https://github.com/${data.github}/${data.title}/pulls)`
   }
     return `Contributing isn't active at this time.`
 }
